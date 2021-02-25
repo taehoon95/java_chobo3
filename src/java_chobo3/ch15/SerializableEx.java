9633583df462a6d7a26dd5d263e28f53802193a6
Taehoon <leeth950304@gmail.com>
2021-02-25 10:23:05 +0900

    역직렬화 적용 2

--- src/java_chobo3/ch15/SerializableEx.java
+++ src/java_chobo3/ch15/SerializableEx.java
@@ -25,8 +25,8 @@ public class SerializableEx {
 			list.add(u1);
 			list.add(u2);
 			
-			oos.writeObject(u1);
-			oos.writeObject(u2);
+//			oos.writeObject(u1);
+//			oos.writeObject(u2);
 			oos.writeObject(list);
 			
 		} catch (FileNotFoundException e) {
