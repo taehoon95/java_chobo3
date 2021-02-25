bb2d756377789318aab706a78931a3a84c761173
Taehoon <leeth950304@gmail.com>
2021-02-25 10:05:49 +0900

    역직렬화 추가

--- src/java_chobo3/ch15/DeSerializableEx.java
+++ src/java_chobo3/ch15/DeSerializableEx.java
@@ -1,9 +1,32 @@
 package java_chobo3.ch15;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 public class DeSerializableEx {
-
-	public static void main(String[] args) {
-
+	// 역직렬화
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String fileName = "UserInfo.ser";
+		
+		try(FileInputStream fis = new FileInputStream(fileName);
+				BufferedInputStream bis = new BufferedInputStream(fis);
+				ObjectInputStream ois = new ObjectInputStream(bis)){
+			
+			UserInfo u1 = (UserInfo) ois.readObject();
+			UserInfo u2 = (UserInfo) ois.readObject();
+			ArrayList<UserInfo> list = (ArrayList) ois.readObject();
+			
+			System.out.println(u1);
+			System.out.println(u2);
+			System.out.println(list);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/java_chobo3/ch15/SerializableEx.java
+++ src/java_chobo3/ch15/SerializableEx.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 public class SerializableEx {
 
 
-
 	public static void main(String[] args) {
 		String fileName = "UserInfo.ser";
 		
